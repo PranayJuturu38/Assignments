3b2164784d0ad7b9ddca8f2e4212099d177afb04
PranayJuturu <pranayreddy38@gmail.com>
2021-07-20 10:26:56 +0530

    Assignment 2

---
+++ src/AssignmentsKPI/Assignment2.java
@@ -0,0 +1,51 @@
+package AssignmentsKPI;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Assignment2 {
+    static void  map(int n,String[] keys,int[] arr,String s){
+        HashMap<String,Integer> hm = new HashMap<String,Integer>();
+        for(int i=0;i<n;i++){
+            hm.put(keys[i],arr[i]);
+        }
+        System.out.println("Initial mapping"+hm);
+        if(!hm.containsKey(s)){
+            System.out.println("-1");
+        }
+        else
+            System.out.println(hm.get(s));
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Taking the number of keys as input
+        System.out.println("Enter the number of keys");
+        int num = sc.nextInt();
+
+        //Taking the keys as input
+        System.out.println("Enter the keys:");
+        String[] keys =new String[num];
+        for(int i=0;i<num;i++){
+            keys[i] = sc.next();
+        }
+        //Displaying the keys entered
+        System.out.println("Keys entered:");
+        for(int i=0;i<num;i++){
+            System.out.println(keys[i]);
+        }
+        //Taking the values for the keys
+        System.out.println("Enter the values for the keys");
+        int[] array = new int[num];
+        for(int i=0;i<num;i++){
+            array[i]=sc.nextInt();
+        }
+        //Taking the String s to search the required key
+        System.out.println("Enter the value of S:");
+        String str = sc.next();
+
+        //Passing the inputs as parameters
+        map(num,keys,array,str);
+    }
+}
