5296683cac1f74b089068193cd30ca4955027aeb
PranayJuturu <pranayreddy38@gmail.com>
2021-07-20 10:18:57 +0530

    Assignment 1: For a given N natural numbers, this function returns the sum of even numbers and odd numbers less than N

---
+++ src/AssignmentsKPI/Assignment1.java
@@ -0,0 +1,28 @@
+package AssignmentsKPI;
+import java.util.Scanner;
+
+public class Assignment1 {
+    static void getSum(int N) {
+
+        int evenSum = 0;
+        int oddSum = 0;
+        for (int i = 1; i <= N; i++) {
+            if (i%2 == 0)
+                evenSum += i;
+            else
+                oddSum += i;
+
+        }
+        System.out.println("Sum of even numbers:"+evenSum);
+        System.out.println("Sum of odd numbers:"+oddSum);
+
+    }
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of Natural numbers:");
+        int num = sc.nextInt();
+        getSum(num);
+
+
+    }
+}
